de508373343042e962d0c843e2f33859e191133d
hyunsu00 <hyun955807@naver.com>
2021-07-15 19:13:30 +0900

    imageMagickSample01 윈도우 빌드 추가

--- imageMagickSample01/imageMagickSample01.cpp
+++ imageMagickSample01/imageMagickSample01.cpp
@@ -4,6 +4,30 @@
 #include <iostream> // std::cout
 #include <vector> // std::vector
 #include <chrono> // std::chrono
+#ifdef _WIN32
+#include <atlconv.h> // USES_CONVERSION, A2W
+#endif
+
+void _SetImageMagickDllPath(const std::string& exePath)
+{
+#ifdef _WIN32
+	USES_CONVERSION;
+	std::wstring wexePath = A2W(exePath.c_str());
+	wchar_t drive[_MAX_DRIVE] = { 0, }; // 드라이브 명
+	wchar_t dir[_MAX_DIR] = { 0, }; // 디렉토리 경로
+	_wsplitpath_s(wexePath.c_str(), drive, _MAX_DRIVE, dir, _MAX_DIR, nullptr, 0, nullptr, 0);
+	std::wstring exeDir = std::wstring(drive) + dir;
+
+	size_t requiredSize;
+	_wgetenv_s(&requiredSize, nullptr, 0, L"PATH");
+	std::vector<wchar_t> envPath(requiredSize, 0);
+	_wgetenv_s(&requiredSize, &envPath[0], requiredSize, L"PATH");
+
+	std::wstring addEnvPath = std::wstring(exeDir.c_str()) + L"ImageMagick;";
+	addEnvPath += &envPath[0];
+	_wputenv_s(L"PATH", addEnvPath.c_str());
+#endif
+}
 
 int CropImageToTiles(const Magick::Image& image, const int cropWidth, const int cropHeight, std::vector<Magick::Image>& imageVector)
 {
@@ -33,9 +57,9 @@ int CropImageToTiles(const Magick::Image& image, const int cropWidth, const int 
 	return 0;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    Magick::InitializeMagick(*argv);
+    Magick::InitializeMagick(argv[0]);
 
     Magick::Image image;
 	std::vector<Magick::Image> imageVector;
