2267b80d56fbb89bcc23b34abb8a4b51577041fd
hyunsu00 <hyun955807@naver.com>
2021-07-16 10:39:22 +0900

    MagickCLI 예제 추가

--- imageMagickSample04/imageMagickSample04.cpp
+++ imageMagickSample04/imageMagickSample04.cpp
@@ -6,13 +6,60 @@
 #include "common.h"
 
 #ifdef _WIN32
+#include "MagickCore/studio.h"
+#include "MagickWand/MagickWand.h"
+int Main(int argc, char* argv[])
+{
+	std::string samplesDir = common::pathAddSeparator(common::getDir(argv[0]) + "samples");
+	std::string resultDir = common::pathAddSeparator(common::getDir(argv[0]) + "result");
+
+	//  magick -size 100x100 xc:red ( rose: -rotate -90 ) +append show:
+	/*const char* args[] = { "-size", "100x100", "xc:red",
+					 "(", "rose:", "-rotate", "-90", ")",
+					 "+append", "show:", NULL 
+	};*/
+
+	// magick convert lena.jpg -resize 50 % lena.png
+	/*std::string inputFile = samplesDir + "lena.jpg";
+	std::string outputFile = resultDir + "lena.png";
+	const char* args[] = { 
+		inputFile.c_str(), "-resize", "50%%", outputFile.c_str(), NULL
+	};*/
+
+	// magick convert -crop 300x200 logo: ./result/cropped_%d.png
+	std::string inputFile = samplesDir + "chessboard.png";
+	std::string outputFile = resultDir + "cropped_%d.png";
+	const char* args[] = { 
+		inputFile.c_str(), "-crop", "449x465", outputFile.c_str(), NULL
+	};
+
+	int arg_count;
+	for (arg_count = 0; args[arg_count] != (char*)NULL; arg_count++);
+
+	MagickCoreGenesis(argv[0], MagickFalse);
+	{
+		MagickCLI* cli_wand = AcquireMagickCLI((ImageInfo*)NULL, (ExceptionInfo*)NULL);
+
+		ProcessCommandOptions(cli_wand, arg_count, const_cast<char**>(args), 0);
+
+		/* Note use of 'True' to report all exceptions - including non-fatals */
+		if (CLICatchException(cli_wand, MagickTrue) != MagickFalse) {
+			fprintf(stderr, "Major Error Detected\n");
+		}
+
+		cli_wand = DestroyMagickCLI(cli_wand);
+	}
+	MagickCoreTerminus();
+
+	return 0;
+}
 #else
 #include <stdio.h> // system
 #include <string.h>	// strdup
 #include <libgen.h>	// dirname
 #include <unistd.h> // execl
 #include <sys/wait.h> // wait
-int linuxMain(int argc, char* argv[])
+int Main(int argc, char* argv[])
 {
 	char* exePath = strdup(argv[0]);
 	std::string exeDir = dirname(exePath);
@@ -52,9 +99,5 @@ int linuxMain(int argc, char* argv[])
 
 int main(int argc, char** argv)
 {
-#ifdef _WIN32
-	return 0;
-#else
-	return linuxMain(argc, argv);
-#endif
+	return Main(argc, argv);
 }
